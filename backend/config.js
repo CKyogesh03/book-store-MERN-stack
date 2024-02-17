dfa4461cc0293da170dcfde57a28d0d5ca87c3f7
CKyogesh03 <yogeshlogan333@gmail.com>
2024-02-17 17:53:59 +0530

    removing access credentials

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT= 5555;
 
 export const mongoDBURL= 
-'mongodb+srv://root:root@cluster0.u0oeoct.mongodb.net/books-store-MERN?retryWrites=true&w=majority'
+'mongodb+srv://username:password@cluster0.u0oeoct.mongodb.net/books-store-MERN?retryWrites=true&w=majority'
